fcb384d87d4cec1463a0666bba08d7fece8aec01
nimadini <nima.dini@gmail.com>
2014-12-15 00:33:07 -0600

    Whoops! app email address updated to new web domain

--- handlers/message.py
+++ handlers/message.py
@@ -32,9 +32,9 @@ class MessageHandler(webapp2.RequestHandler):
         msg = self.request.get('msg')
 
         prop = usr.get_prop()
-        url = 'http://teammate-beta.appspot.com/home?user=' + usr.id
+        url = 'http://teammate-app.appspot.com/home?user=' + usr.id
 
-        mail.send_mail(sender="Teammate <info@teammate-beta.appspotmail.com>",
+        mail.send_mail(sender="Teammate <info@teammate-app.appspotmail.com>",
                        to=target_email,
                        subject="New message from " + usr.given_name + ' ' + usr.surname,
                        body="""
